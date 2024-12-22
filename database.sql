c22654b1cde9fe00866649c84c5dceb100d1091c
piyush dixit <dixitpiyushk@gmail.com>
2024-12-22 20:43:20 +0530

    added functionality wwhich loads data into table using pandas

---
+++ database.sql
@@ -0,0 +1,11 @@
+
+create database if not exists  webscrape;
+
+CREATE TABLE Details (
+`Rank` INT,
+  Name VARCHAR(200),
+  Revenue VARCHAR(200),
+ Industry VARCHAR(200),
+   Employees VARCHAR(200),
+   Headquarters VARCHAR(200)
+ );
